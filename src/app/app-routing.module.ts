728a582de04d39317f0ef41397a0f098a934145c
Cran-Barry <82969030+Cran-Barry@users.noreply.github.com>
2022-11-02 16:26:01 -0400

    Added blank components and routing for the page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Components/Pages/homepage/homepage.component';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { NotfoundComponent } from './Components/Pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+
+  {
+    path:'',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+
+  {
+    //* is the wildcard character
+    path: "**",
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Components/Pages/homepage/homepage.component';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { RegisterComponent } from './Components/Pages/register/register.component';
+import { NotfoundComponent } from './Components/Pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
