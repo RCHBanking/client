f9c19590570ff03c951d571feb4c906c5fadf6a6
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-11-18 14:32:38 -0800

    transfer complete, need dropdown menu for 'from account'

--- src/app/Services/transaction/transaction.service.ts
+++ src/app/Services/transaction/transaction.service.ts
@@ -11,6 +11,11 @@ export class TransactionService {
 
   constructor(private http: HttpClient) { }
 
+  public amount?: number;
+  public description?: string;
+  public fromAccountId?: number;
+  public toAccountId?: number;
+
   public header = {
     "Authorization": `Bearer ${localStorage.getItem("token")}`
   }
@@ -32,5 +37,21 @@ export class TransactionService {
 
   getExpenseSum(id: number): Observable<number> {
     return this.http.get<number>(`${environment.apiUrl}/api/transaction/view-expense-sum/${id}`, this.requestOptions);
+  }
+
+  createTransfer(): void {
+    this.http.post<Transaction>(`${environment.apiUrl}/api/transaction/create-transfer/1&2`, {"amount": 20.00, "description": "Transfer Test", "transactionType": 2}, this.requestOptions);
+  }
+
+  registerCustomer(firstname: string, surname: string, email: string, username: string, password: string){    
+    return this.http.post<any>(environment.apiUrl + `/api/customer/register`, {firstname: `${firstname}`, surname: `${surname}`, email: `${email}`, username: `${username}`, password: `${password}`}).subscribe(data => {
+      console.log(data)
+    });
+  }
+
+  registerTransfer(amount: number, description: string, fromId: number, toId: number){    
+    return this.http.post<any>(environment.apiUrl + `/api/transaction/create-transfer/${fromId}&${toId}`, {amount: `${amount}`, description: `${description}`, transactionType: 2}, this.requestOptions).subscribe(data => {
+      console.log(data)
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +13,8 @@ import { SummaryComponent } from './Components/Pages/summary/summary.component';
 import { AccountsComponent } from './Components/accounts/accounts.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { AccountDetailComponent } from './Components/account-detail/account-detail.component';
+import { TransferComponent } from './Components/Pages/transfer/transfer.component';
+
 
 @NgModule({
   declarations: [
@@ -25,12 +27,15 @@ import { AccountDetailComponent } from './Components/account-detail/account-deta
     AccountsComponent,
     NavbarComponent,
     AccountDetailComponent,
+    TransferComponent
+    
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
