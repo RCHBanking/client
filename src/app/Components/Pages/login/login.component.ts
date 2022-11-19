91f42b622ab18e41868d643647c7c33ee34704e2
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-11-19 12:16:51 -0800

    merge

--- src/app/Components/Pages/login/login.component.ts
+++ src/app/Components/Pages/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 
-import { AuthService } from '../../Services/auth/auth.service';
+import { AuthService } from '../../../Services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
